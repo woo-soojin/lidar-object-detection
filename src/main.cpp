d96bfab471152302acde1fbf0b8c78bf68dd6847
woo-soojin <soojin.woo@snu.ac.kr>
2023-10-19 13:40:10 +0900

    ROS package

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,61 @@
  * Date: 05/25/2019
  */
 
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <thread>
+
 #include "render/render.h"
 #include "processPointClouds.h"
-// using templates for processPointClouds so also include .cpp to help linker
 #include "processPointClouds.cpp"
 
+pcl::PointCloud<pcl::PointXYZI>::Ptr input_cloud(new pcl::PointCloud<pcl::PointXYZI>()); // TODO
+
+void drawRangeCircle(pcl::visualization::PCLVisualizer::Ptr viewer, float range1, float range2, float range3)
+{
+    pcl::ModelCoefficients coeffs;
+    coeffs.values.clear ();
+    coeffs.values.push_back (0);
+    coeffs.values.push_back (0);
+    coeffs.values.push_back (range1);
+    viewer->addCircle(coeffs, "circle");
+    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_REPRESENTATION, pcl::visualization::PCL_VISUALIZER_REPRESENTATION_WIREFRAME, "circle");
+    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_LINE_WIDTH,3, "circle");
+    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 1,0,0, "circle");
+
+    coeffs.values.clear ();
+    coeffs.values.push_back (0);
+    coeffs.values.push_back (0);
+    coeffs.values.push_back (range2);
+    viewer->addCircle(coeffs, "circle2");
+    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_REPRESENTATION, pcl::visualization::PCL_VISUALIZER_REPRESENTATION_WIREFRAME, "circle2");
+    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_LINE_WIDTH,3, "circle2");
+    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 1,1,0, "circle2");
+
+    coeffs.values.clear ();
+    coeffs.values.push_back (0);
+    coeffs.values.push_back (0);
+    coeffs.values.push_back (range3);
+    viewer->addCircle(coeffs, "circle3");
+    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_REPRESENTATION, pcl::visualization::PCL_VISUALIZER_REPRESENTATION_WIREFRAME, "circle3");
+    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_LINE_WIDTH,3, "circle3");
+    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 0,1,0, "circle3");
+}
+
+void drawRangeRectangle(pcl::visualization::PCLVisualizer::Ptr& viewer, float range1, float range2, float range3) {
+    Box rectangle1 = {-range1, -range1, 0.0, range1, range1, 0.0};
+    drawRectangle(viewer, rectangle1, "rectangle1", Color(1.0, 0.0, 0.0), 1.0);
+
+    Box rectangle2 = {-range2, -range2, 0.0, range2, range2, 0.0};
+    drawRectangle(viewer, rectangle2, "rectangle2", Color(1.0, 1.0, 0.0), 1.0);
+
+    Box rectangle3 = {-range3, -range3, 0.0, range3, range3, 0.0};
+    drawRectangle(viewer, rectangle3, "rectangle3", Color(0.0, 1.0, 0.0), 1.0);
+}
+
 void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer, ProcessPointClouds<pcl::PointXYZI>& point_cloud_processor, pcl::PointCloud<pcl::PointXYZI>::Ptr& input_cloud) {
-    renderPointCloud(viewer, input_cloud, "InputCloud");
+    renderPointCloud(viewer, input_cloud, "InputCloud",  Color(0.3, 0.3, 0.3));
 
     // Input point cloud, filter resolution, min Point, max Point
     constexpr float kFilterResolution = 0.2;
@@ -21,43 +69,37 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer, ProcessPointCloud
     const Eigen::Vector4f kMaxPoint(60, 6.5, 4, 1);
     auto filter_cloud = point_cloud_processor.FilterCloud(input_cloud, kFilterResolution, kMinPoint, kMaxPoint);
 
-//    renderPointCloud(viewer, filter_cloud, "FilteredCloud");
-
     constexpr int kMaxIterations = 100;
     constexpr float kDistanceThreshold = 0.2;
     auto segment_cloud = point_cloud_processor.SegmentPlane(filter_cloud, kMaxIterations, kDistanceThreshold);
 
-//    // render obstacles point cloud with red
-//    renderPointCloud(viewer, segment_cloud.first, "ObstacleCloud", Color(1, 0, 0));
-    // render ground plane with green
-//    renderPointCloud(viewer, segment_cloud.second, "GroundCloud", Color(0, 1, 0));
-
-
-    /*** Euclidean clustering ***/
-    // float clusterTolerance, int minSize, int maxSize
     constexpr float kClusterTolerance = 0.35;
     constexpr int kMinSize = 15;
     constexpr int kMaxSize = 600;
     auto cloud_clusters = point_cloud_processor.Clustering(segment_cloud.first, kClusterTolerance, kMinSize, kMaxSize);
 
-    int cluster_ID = 1;
+    int cluster_ID = 2;
     std::vector<Color> colors = {Color(1, 0, 0), Color(0, 0, 1), Color(0.5, 0, 1)};
     int num_of_colors = colors.size();
 
-    Box host_box = {-1.5, -1.7, -1, 2.6, 1.7, -0.4};
-    renderBox(viewer, host_box, 0, Color(0.5, 0, 1), 0.8);
+    Box transporter = {-10.0, -3.5, -1, 10.0, 3.5, -0.4};
+    renderBox(viewer, transporter, 0, Color(1.0, 0.5, 0.0), 1.0);
+
+    Box block = {-12.5, -12.5, -0.4, 12.5, 12.5, 17.6};
+    renderBox(viewer, block, 1, Color(0.8, 0.8, 0.8), 1.0);
+
+    // range
+    drawRangeRectangle(viewer, 15.0, 25.0, 35.0); // 30, 50, 70
 
     constexpr float kBBoxMinHeight = 0.75;
     for(const auto& cluster : cloud_clusters) {
         std::cout << "cluster size ";
         point_cloud_processor.numPoints(cluster);
 
-//        renderPointCloud(viewer, cluster, "ObstacleCloud" + std::to_string(cluster_ID), colors[cluster_ID % num_of_colors]);
-
         Box box = point_cloud_processor.BoundingBox(cluster);
         // Filter out some cluster with little points and shorter in height
         if (box.z_max - box.z_min >= kBBoxMinHeight || cluster->points.size() >= kMinSize * 2) {
-            renderBox(viewer, box, cluster_ID);
+            renderBox(viewer, box, cluster_ID, Color(1.0, 0.0, 0.0), 1.0);
         }
 
         cluster_ID++;
@@ -67,12 +109,12 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer, ProcessPointCloud
 //setAngle: SWITCH CAMERA ANGLE {XY, TopDown, Side, FPS}
 void initCamera(CameraAngle setAngle, pcl::visualization::PCLVisualizer::Ptr& viewer) {
     viewer->setBackgroundColor(0, 0, 0);
-    
+
     // set camera position and angle
     viewer->initCameraParameters();
     // distance away in meters
     int distance = 16;
-    
+
     switch(setAngle) {
         case XY : viewer->setCameraPosition(-distance, -distance, distance, 1, 1, 0); break;
         case TopDown : viewer->setCameraPosition(0, 0, distance, 1, 0, 1); break;
@@ -84,32 +126,44 @@ void initCamera(CameraAngle setAngle, pcl::visualization::PCLVisualizer::Ptr& vi
         viewer->addCoordinateSystem(1.0);
 }
 
-
-int main (int argc, char** argv) {
-    pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
+void run_pcl_viewer(pcl::PointCloud<pcl::PointXYZI>::Ptr input_cloud) {
+    pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("FOC GUI")); // TODO
     CameraAngle setAngle = FPS;
-    initCamera(setAngle, viewer);
+//    initCamera(setAngle, viewer);
+    int distance = 200;
+    viewer->setCameraPosition(0, 0, distance, 1, 0, 1);
 
     ProcessPointClouds<pcl::PointXYZI> point_cloud_processor;
-    std::vector<boost::filesystem::path> stream = point_cloud_processor.streamPcd("../data/pcd/data_2");
-    auto stream_iterator = stream.begin();
-    pcl::PointCloud<pcl::PointXYZI>::Ptr input_cloud;
 
-    while (!viewer->wasStopped()) {
+    while(!viewer->wasStopped()) {
         // Clear viewer
         viewer->removeAllPointClouds();
         viewer->removeAllShapes();
 
-        // Load pcd and run obstacle detection process
-        input_cloud = point_cloud_processor.loadPcd((*stream_iterator).string());
+        // Run obstacle detection process
         cityBlock(viewer, point_cloud_processor, input_cloud);
 
-        stream_iterator++;
-        // keep looping
-        if(stream_iterator == stream.end())
-            stream_iterator = stream.begin();
-
         // viewer spin
-        viewer->spinOnce();
+        viewer->spinOnce(100);
     }
+}
+
+void callback(const sensor_msgs::PointCloud2& msg) {
+    pcl::fromROSMsg(msg, *input_cloud);
+}
+
+int main (int argc, char** argv) {
+    std::thread pclViewer(run_pcl_viewer, input_cloud);
+
+    // Initialize ROS
+    ros::init (argc, argv, "FOC_GUI");
+    ros::NodeHandle nh;
+
+    // Create a ROS subscriber for the input point cloud
+//    ros::Subscriber sub = nh.subscribe("/ouster/points", 1, callback);
+//    ros::Subscriber sub = nh.subscribe("/velodyne_points", 1, callback);
+    ros::Subscriber sub = nh.subscribe("/lidar_total", 1, callback);
+    ros::spin();
+
+    return 0;
 }
--- src/render/render.cpp
+++ src/render/render.cpp
@@ -84,6 +84,22 @@ void renderBox(pcl::visualization::PCLVisualizer::Ptr& viewer, Box box, int id, 
     viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, opacity*0.3, cubeFill);
 }
 
+// Draw wire frame box with filled transparent color
+void drawRectangle(pcl::visualization::PCLVisualizer::Ptr& viewer, Box box, std::string id, Color color, float opacity) // TODO
+{
+    if(opacity > 1.0)
+        opacity = 1.0;
+    if(opacity < 0.0)
+        opacity = 0.0;
+
+//    std::string cube = "box"+std::to_string(id);
+    //viewer->addCube(box.bboxTransform, box.bboxQuaternion, box.cube_length, box.cube_width, box.cube_height, cube);
+    viewer->addCube(box.x_min, box.x_max, box.y_min, box.y_max, box.z_min, box.z_max, color.r, color.g, color.b, id);
+    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_REPRESENTATION, pcl::visualization::PCL_VISUALIZER_REPRESENTATION_WIREFRAME, id);
+    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, color.r, color.g, color.b, id);
+    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, opacity, id);
+}
+
 void renderBox(pcl::visualization::PCLVisualizer::Ptr& viewer, BoxQ box, int id, Color color, float opacity)
 {
 	if(opacity > 1.0)
